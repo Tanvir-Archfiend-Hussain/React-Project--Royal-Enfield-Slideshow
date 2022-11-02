034ede0f778d69dbb216fbf5e9106c9a94943047
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-02 22:51:32 +0000

    Main Commit

---
+++ royal-enfield-slideshow/src/components/SliderData.js
@@ -0,0 +1,32 @@
+export const SliderData = [
+    {
+        image: 'https://static.india.com/wp-content/uploads/2021/09/Classic350_16.jpg?impolicy=Medium_Widthonly&w=700&h=467%20widht='
+    },
+    {
+        image: 'https://static.india.com/wp-content/uploads/2021/12/Classic350_5.jpg?impolicy=Medium_Widthonly&w=700&h=467%20widht='
+    },
+    {
+        image: 'https://cloudfront-us-east-1.images.arcpublishing.com/octane/BVTYJX6NJVC57LFXQCC3L6FGMY.jpg'
+    },
+    {
+        image: 'https://www.webbikeworld.com/wp-content/uploads/2022/07/Royal-Enfield-Resurgence.jpeg'
+    },
+    {
+        image: 'https://gomechanic.in/blog/wp-content/uploads/2020/05/Royal-Enfield-Motorcycles-The-History-of-a-Cult-01.jpg'
+    },
+    {
+        image: 'https://media.gq-magazine.co.uk/photos/5d13a4e1539cb36fa2223090/master/w_1600%2Cc_limit/classic-350-gray-female-international.jpg'
+    },
+    {
+        image: 'https://imgd.aeplcdn.com/0x0/n/cw/ec/101487/classic-350-2021-right-rear-three-quarter-9.jpeg?isig=0'
+    },
+    {
+        image: 'https://pbs.twimg.com/media/Fabjx3daQAEZamr?format=jpg&name=large'
+    },
+    {
+        image: 'https://gumlet.assettype.com/evoindia%2F2022-06%2Faf78e980-8310-4917-aaa2-15d8be40c12b%2FRoyal_Enfield_Customs__1_69.jpg?format=auto'
+    },
+    {
+        image: 'https://cdn.motor1.com/images/mgl/P3GoeX/s1/sg650lf34.jpg'
+    }
+]
